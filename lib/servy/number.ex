e7e77441b2c0f7e885095b62325b028f84ca03e1
cabmak <morin700@live.ca>
2021-01-22 11:35:56 -0500

    Number.ex

---
+++ lib/servy/number.ex
@@ -0,0 +1,22 @@
+defmodule Servy.Number do
+  def add_number(variable_a, variable_b) do
+    variable_a + variable_b
+  end
+
+  def div_number(coco, poulette)  do
+
+    coco / poulette
+
+end
+
+  def multiply_number(a, b) do
+
+    a * b
+  end
+
+  def sub_number(ralph, john) do
+
+    ralph - john
+
+  end
+end
